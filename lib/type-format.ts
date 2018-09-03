2d01ce7e22b88a45b37481658edc751e46485d2e
Vita Tauer <vita@nothrow.cz>
2018-09-03 18:34:33 +0200

    Implemented formatType

--- lib/type-format.ts
+++ lib/type-format.ts
@@ -1,16 +1,40 @@
-import { IAssemblyQualifiedName } from "./type-parse";
+import { IAssemblyQualifiedName, parseAssemblyQualifiedName } from "./type-parse";
 
 export enum FormatFlags
 {
-    IncludeNamespaces = 0,
-    ExcludeNamespaces = 1
+    ExcludeNamespaces = 0,
+    IncludeNamespaces = 1
 }
 
 /**
  * @method: parses c#'s "typeof(Type).FullName" back to something that can be read.
- * @param aqn assembly qualified name to parse
+ * @param aqn parsed assembly qualified name to format
  */
-export function formatType(aqn: IAssemblyQualifiedName) : string {
-    return "";
+export function formatType(aqn: IAssemblyQualifiedName, formatFlags : FormatFlags = 0) : string {
+    let rv = '';
+    if ((formatFlags & FormatFlags.IncludeNamespaces) == FormatFlags.IncludeNamespaces)
+    {
+        rv += aqn.namespace;
+        if (aqn.namespace)
+            rv += '.';
+    }
+
+    rv += aqn.typeName;
+    if (aqn.templateParameters)
+    {
+        rv += '<';
+
+        for(let i = 0; i < aqn.templateParameters.length; i++)
+        {
+            if (i > 0)
+                rv += ',';
+
+            rv += formatType(aqn.templateParameters[i], formatFlags);
+        }
+
+        rv += '>';
+    }
+
+    return rv;
 }
 
